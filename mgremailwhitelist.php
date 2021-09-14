bceb1bfa69a8444f55a9f3bb9d0edfcc1fb48f13
Rene Pilz <rene@pilz.cc>
2021-09-14 21:04:14 +0200

    first steps, something taken from https://plugins.trac.wordpress.org/browser/cookie-notice/trunk/cookie-notice.php

--- mgremailwhitelist.php
+++ mgremailwhitelist.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Plugin Name:       Manage eMail Whitelist 
  * Plugin URI:        https://fungus.at/plugins/mgremailwhitelist/
@@ -13,3 +14,54 @@
  * Text Domain:       mgremailwhitelist
  * Domain Path:       /languages
  */
+
+// exit if accessed directly
+if ( ! defined( 'ABSPATH' ) )
+        exit;
+
+
+/**
+ * Manage eMail Whitelist main class
+ *
+ * @class ManageEMailWhitelist
+ * @version 0.0.1
+ */
+class ManageEMailWhitelist {
+	private static $_instance;
+
+
+	/**
+	 * Disable object cloning.
+	 */
+	public function __clone() {}
+
+	/**
+	 * Disable unserializing of the class.
+	 */
+	public function __wakeup() {}
+
+	/**
+	 * Main plugin instance.
+	 *
+	 * @return object
+	 */
+	public static function instance() {
+		if ( self::$_instance === null ) {
+			self::$_instance = new self();
+			add_action( 'plugins_loaded', array( self::$_instance, 'load_textdomain' ) );
+			self::$_instance->includes();
+			
+		}
+		return self::$_instance;
+	}
+
+
+	/**
+	 * Include required files.
+	 *
+	 * @return void
+	 */
+	private function includes() {
+		// include_once( plugin_dir_path( __FILE__ ) . 'includes/bot-detect.php' );
+	}
+

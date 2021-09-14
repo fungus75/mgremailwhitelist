e02c3b9a65b6f89e050ed319e70067d84397ea1e
Rene Pilz <rene@pilz.cc>
2021-09-14 20:42:10 +0200

    first commit

---
+++ mgremailwhitelist.php
@@ -0,0 +1,15 @@
+/**
+ * Plugin Name:       Manage eMail Whitelist 
+ * Plugin URI:        https://fungus.at/plugins/mgremailwhitelist/
+ * Description:       Manage eMail Whitelists for a given postgrey-installation
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rene Pilz
+ * Author URI:        https://pilz.cc/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       mgremailwhitelist
+ * Domain Path:       /languages
+ */
